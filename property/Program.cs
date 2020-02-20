0bca4e100bd0bb890b0a566702073c8fd2ceace4
Rohithapatchava <60814260+Rohithapatchava@users.noreply.github.com>
2020-02-20 16:26:50 +0530

    Initial commit

---
+++ property/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace property
+{
+    class Program
+    {
+        static void main(String[] args)
+        {
+           demo dem = new demo();
+           dem.Age = 22;
+            Console.WriteLine(dem.Age);
+        }
+    }
+
+}
+    class demo
+    {
+        int age;
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                age = value;
+            }
+        }
+        
+}
